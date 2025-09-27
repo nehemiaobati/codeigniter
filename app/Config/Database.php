4f7efb64369e74670419538eb44f09f8975c1e19
Nehemia <nehemia.obati@kingsway.co.ke>
2025-09-27 23:08:42 +0300

    feat: Update default database credentials to empty strings
    The default database credentials in the `app/Config/Database.php` file have been updated to empty strings. This change is made to prevent accidental use of placeholder credentials in production environments and to encourage users to explicitly configure their database connection details.

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -27,9 +27,9 @@ class Database extends Config
     public array $default = [
         'DSN'          => '',
         'hostname'     => '127.0.0.1',
-        'username'     => 'codeigniter',
-        'password'     => '12345678',
-        'database'     => 'server_codeigniter',
+        'username'     => '',
+        'password'     => '',
+        'database'     => '',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
